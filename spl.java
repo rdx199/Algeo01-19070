8234cb9f5688b8d075441b23d278d5b6b58964e3
ahandnr <68496198+ahandnr@users.noreply.github.com>
2020-09-28 23:26:44 +0700

    Update spl.java
    rapihin

--- spl.java
+++ spl.java
@@ -1,99 +1,59 @@
-package com.kuliah;
+package com.company;
 
 import java.util.Scanner;
 
 public class Spl {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Sistem Persamaan Linear\n\nBUAT MATRIKS :");
+        // input matriks
         int jumlahSPL = insertJumlahSPL();
         int jumlahVar = insertJumlahVar();
-
         double[][] matriks = new double[jumlahSPL][jumlahVar + 1];
-
         bacaValue(matriks, jumlahSPL, jumlahVar);
+
         System.out.println("Matriks: \n");
-        printMatriks(matriks, jumlahSPL, jumlahVar);
-        System.out.println("");
-        System.out.println("Matriks gauss jordan: \n");
-        gaussjordan(matriks, jumlahSPL, jumlahVar);
-        printMatriks(matriks, jumlahSPL, jumlahVar);
+        printMatriks(matriks);
     }
 
     public static int insertJumlahSPL() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Masukkan jumlah baris (m): ");
+        System.out.print("Masukkan jumlah persamaan: ");
         return scanner.nextInt();
     }
 
     public static int insertJumlahVar() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Masukkan jumlah kolom (n): ");
+        System.out.print("Masukkan jumlah variabel: ");
         return scanner.nextInt();
     }
 
-    public static double[][] bacaValue (double[][] matriks, int jumlahSPL, int jumlahVar) {
+    public static void bacaValue (double[][] M, int jumlahSPL, int jumlahVar) {
         Scanner scanner = new Scanner(System.in);
 
         for (int i=0; i<jumlahSPL; i++) {
             for (int j=0; j<jumlahVar; j++) {
                 System.out.print("Masukkan matriks"+ "[" + (i+1) + "]"  + "[" + (j+1) + "] : ");
-                matriks[i][j]=scanner.nextDouble();
+                M[i][j]=scanner.nextDouble();
             }
 
             System.out.print("Masukkan solusi SPL-" + (i+1) + " : ");
-            matriks[i][jumlahVar]=scanner.nextDouble();
-        }
-
-        return matriks;
-    }
-
-    public static void printMatriks(double[][] matriks, int jumlahSPL, int jumlahVar) {
-        for (int i=0; i<jumlahSPL; i++) {
-            for (int j=0; j<jumlahVar; j++) {
-                System.out.print(matriks[i][j]+" ");
-            }
-
-            System.out.print(matriks[i][jumlahVar]);
-            System.out.println("");
+            M[i][jumlahVar]=scanner.nextDouble();
         }
     }
 
-    public static double[][] gauss (double[][] matriks, int jumlahSPL, int jumlahVar){
-        int i, j, k, n;
-        for (i = 0; i < jumlahSPL; i++){
-            double temp = matriks[i][i];
-            for (j = 0; j < jumlahVar+1; j++){
-                matriks[i][j] = matriks[i][j]/temp;
+    public static void printMatriks(double[][] M) {
+        int brs = M.length;
+        int kol = M[0].length;
+        for (int i = 0; i < brs; i++) {
+            System.out.print("[\t");
+            for (int j = 0; j < kol; j++) {
+                System.out.print(M[i][j] + "\t");
             }
-            for (k = 0; k < jumlahSPL; k++){
-                temp = matriks[k][i];
-                if (i != k){
-                    for(n = 0; n < jumlahVar+1; n++){
-                        matriks[k][n] = matriks[k][n] - (temp * (matriks[i][n]));
-                    }
-                }
-            }
-        }
-        return matriks;
-    }
-
-    public static void gaussjordan (double[][] matriks, int jumlahSPL, int jumlahVar){
-        int i, j, k, n;
-        for (i = 0; i < jumlahSPL; i++){
-            double temp = matriks[i][i];
-            for (j = 0; j < jumlahVar+1; j++){
-                matriks[i][j] = matriks[i][j]/temp;
-            }
-            for (k = 0; k < jumlahSPL; k++){
-                temp = matriks[k][i];
-                if (i != k){
-                    for(n = 0; n < jumlahVar+1; n++){
-                        matriks[k][n] = matriks[k][n] - (temp * (matriks[i][n]));
-                    }
-                }
-            }
+            System.out.println("\t]");
         }
 
     }
